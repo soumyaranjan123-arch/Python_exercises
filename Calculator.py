af1f065fd63bc720f345e06ee80f61c0a7a54fa7
Soumyaranjan Mandal <ranjansoumya33479@gmail.com>
2024-09-14 13:41:18 +0530

    Updated Calculator

--- Calculator.py
+++ Calculator.py
@@ -1,7 +1,7 @@
 print('Welcome to Calculator built in Python.')
 
-first_num = int(input('Enter your First_Number: '))
-second_num = int(input('Enter your Second_Number: '))
+first_num = float(input('Enter your First_Number: '))
+second_num = float(input('Enter your Second_Number: '))
 
 operator = input('Input a mathematical operator to perform certain operation: ')
 
@@ -16,10 +16,16 @@ elif operator == '*':
     result = first_num * second_num
 elif operator == '%':
     print('You want to to do Modular Division,that\'s Intresting.')
-    result = first_num % second_num
+    if second_num != 0:
+        result = first_num % second_num
+    else:
+        print('Not divisible by 0')
 elif operator == '/':
     print('You want to Divide,that\'s Intresting.')
-    result = first_num / second_num
+    if second_num != 0:
+        result = first_num / second_num
+    else:
+        print('Not divisible by 0')
 elif operator == '**':
     print('You want to Raise to the Power,that\'s Intresting.')
     result = first_num ** second_num
